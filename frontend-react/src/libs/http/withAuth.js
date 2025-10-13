b091847e9d6d9612593166056aa1d0d184478efc
Bre-wub <brwubet@gmail.com>
2025-10-13 14:20:14 +0300

    ...

--- frontend-react/src/libs/http/withAuth.js
+++
@@ -1,8 +0,0 @@
-import { useAuthStore } from '@/features/auth/stores/auth.store.js';
-
-export function withAuthHeaders(headers = {}) {
-  const token = useAuthStore.getState().accessToken;
-  return token ? { ...headers, Authorization: `Bearer ${token}` } : headers;
-}
-
-
